bc33b472c43f07b35fddaee05969d33da50085ad
felipeirah <felipe.junior.tr@gmail.com>
2026-03-04 10:47:38 -0300

    chore: estruturando o backend

---
+++ backend/src/server.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+console.log("INICIANDO SERVIDOR...");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API rodando 🚀");
+});
+
+app.get("/health", (req, res) => {
+  return res.status(200).json({ status: "ok" });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
