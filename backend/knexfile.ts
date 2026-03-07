d3c979ec1202243c7b03df7ae374f3a4f1fe87ca
Kaio <kaiohtf38@gmail.com>
2026-03-07 17:56:08 -0300

    chore(backend): configure knex and typescript setup

---
+++ backend/knexfile.ts
@@ -0,0 +1,59 @@
+import type { Knex } from "knex";
+
+require('dotenv').config({ path: '.env.development' });
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: "pg",
+    connection: {
+      database: String(process.env.DATABASE_NAME),
+      user: String(process.env.DATABASE_USERNAME),
+      password: String(process.env.DATABASE_PASSWORD)
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: "./migrations"
+    }
+  },
+
+  staging: {
+    client: process.env.DATABASE_CLIENT,
+    connection: {
+      database: String(process.env.DATABASE_NAME),
+      user: String(process.env.DATABASE_USERNAME),
+      password: String(process.env.DATABASE_PASSWORD)
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: "./migrations"
+    }
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: "./migrations"
+    }
+  }
+
+};
+
+module.exports = config;
