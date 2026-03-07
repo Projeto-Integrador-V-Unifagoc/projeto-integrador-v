88f72d56ba302d47f71692d18c792b13645923f4
Kaio <kaiohtf38@gmail.com>
2026-03-07 17:56:25 -0300

    feat(backend): add initial database migrations

---
+++ backend/migrations/20260307200505_tabela_usuarios.ts
@@ -0,0 +1,17 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable('usuarios', (table) => {
+        table.uuid('id').primary()
+        table.string('email').notNullable()
+        table.string('password', 255).notNullable()
+        table.timestamp('created_at').notNullable()
+        table.timestamp('updated_at').notNullable()
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTableIfExists('usuarios')
+}
