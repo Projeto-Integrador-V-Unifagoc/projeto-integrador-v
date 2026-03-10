e0477f20dcf2ea50b7564da64fe8ec9a0382d3cf
João Pedro Vidal <contatojoaopedrovidal@gmail.com>
2026-03-10 19:35:07 -0300

    feat:  Adicionando models para tipagem de entidades

---
+++ backend/src/Modules/core-gestao-de-alunos/models/Pessoa.ts
@@ -0,0 +1,11 @@
+export interface Pessoa {
+    cpf: string
+    nome: string
+    data_nascimento: string
+    logradouro: string
+    numero: number
+    bairro: string
+    cidade: string
+    estado: string
+    cep: string
+}
