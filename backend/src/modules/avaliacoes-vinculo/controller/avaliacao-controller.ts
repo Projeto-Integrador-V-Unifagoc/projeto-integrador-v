430e4ed3add25bc3d9a4d16b9839b1dcd36dde01
Seu Nome <leonardoadmpj@gmail.com>
2026-03-09 23:42:59 -0300

    feat: adiciona endpoint PUT para atualização de avaliações

--- backend/src/modules/avaliacoes-vinculo/controller/avaliacao-controller.ts
+++ backend/src/modules/avaliacoes-vinculo/controller/avaliacao-controller.ts
@@ -1,1 +1,31 @@
-import type { Avaliacao } from '../models/interface.js';
+import type { Request, Response } from "express";
+import { AvaliacaoService } from "../services/avaliacao-service.js";
+
+export class AvaliacaoController {
+
+  private service = new AvaliacaoService();
+
+  atualizar = async (req: Request, res: Response) => {
+
+    try {
+
+      const { id } = req.params;
+
+      const avaliacao = await this.service.atualizar(
+        Number(id),
+        req.body
+      );
+
+      return res.json(avaliacao);
+
+    } catch (error: any) {
+
+      return res.status(400).json({
+        erro: error.message
+      });
+
+    }
+
+  }
+
+}
