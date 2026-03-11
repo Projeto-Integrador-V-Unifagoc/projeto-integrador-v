83daf97d1d04c43578dfa9f8f872e374f4c4104b
Seu Nome <leonardoadmpj@gmail.com>
2026-03-10 23:58:21 -0300

    fix: ajuste do controller.ts

--- backend/src/modules/avaliacoes-vinculo/controller/avaliacao-controller.ts
+++ backend/src/modules/avaliacoes-vinculo/controller/avaliacao-controller.ts
@@ -1,31 +1,21 @@
 import type { Request, Response } from "express";
-import { AvaliacaoService } from "../services/avaliacao-service.js";
+import { AvaliacaoService } from "../services/avaliacao-service"; // Removido .js
 
-export class AvaliacaoController {
+const service = new AvaliacaoService();
 
-  private service = new AvaliacaoService();
+export const atualizar = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
 
-  atualizar = async (req: Request, res: Response) => {
+    const avaliacao = await service.atualizar(
+      Number(id),
+      req.body
+    );
 
-    try {
-
-      const { id } = req.params;
-
-      const avaliacao = await this.service.atualizar(
-        Number(id),
-        req.body
-      );
-
-      return res.json(avaliacao);
-
-    } catch (error: any) {
-
-      return res.status(400).json({
-        erro: error.message
-      });
-
-    }
-
+    return res.json(avaliacao);
+  } catch (error: any) {
+    return res.status(400).json({
+      erro: error.message
+    });
   }
-
-}
+};
