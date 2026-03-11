999506c2245e81b88839782e73d0b2b27f4cccab
João Pedro Vidal <contatojoaopedrovidal@gmail.com>
2026-03-11 02:28:23 -0300

    refactor: pagina de cadastro de alunos refatorada, melhorando a UX e realizando conexão com o backend

--- frontend/src/Pages/Alunos/CadastroAlunos.tsx
+++ frontend/src/Pages/Alunos/CadastroAlunos.tsx
@@ -1,17 +1,24 @@
-import { Grid, Stack } from "@mui/material";
 import { useState } from "react";
-import axios from "axios";
-import { Card } from "../../components/Card";
+
 import Container from "../../components/Container";
 import TextField from "../../components/TextField";
 import Button from "../../components/Button";
+import { Card } from "../../components/Card";
 
-export default function CadastroAlunos(){
+import { Alert, Grid, IconButton, Stack } from "@mui/material";
+
+import { useAluno } from "../../hooks/use-aluno";
+import { MapPin } from "lucide-react";
+import { useViaCep } from "../../hooks/use-cep";
+import { alunoSchema } from "../../validators/aluno-schema";
+
+export default function CadastroAlunos() {
 
   const initialForm = {
     nome: "",
     cpf: "",
     dataNascimento: "",
+    tipoUsuario: "",
     logradouro: "",
     numero: "",
     bairro: "",
@@ -26,7 +33,29 @@ export default function CadastroAlunos(){
   }
 
   const [form, setForm] = useState(initialForm)
-  const [loading, setLoading] = useState(false)
+  const [alerta, setAlerta] = useState<{
+    tipo: "success" | "error"
+    mensagem: string
+  } | null>(null)
+  const [erros, setErros] = useState<Record<string, string>>({})
+  const { carregando, criarAluno } = useAluno()
+  const { carregando: isCarregando, buscarCep } = useViaCep()
+
+  async function buscarEnderecoPeloCep() {
+    const data = await buscarCep(form.cep)
+
+    if (!data) {
+      return
+    }
+
+    setForm((prev) => ({
+      ...prev,
+      logradouro: data.logradouro,
+      bairro: data.bairro,
+      cidade: data.localidade,
+      estado: data.estado
+    }))
+  }
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
     const { name, value } = e.target
@@ -37,202 +66,331 @@ export default function CadastroAlunos(){
     })
   }
 
-  async function buscarCep(cep: string){
+  async function handleSubmit() {
+    console.log('clicou no botão')
 
-    if(cep.length !== 8) return
-
-    try{
-
-      const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`)
-
-      const data = response.data
-
-      setForm((prev)=>({
-        ...prev,
-        logradouro: data.logradouro || "",
-        bairro: data.bairro || "",
-        cidade: data.localidade || "",
-        estado: data.uf || ""
-      }))
-
-    }catch(err){
-      console.log("Erro ao buscar CEP")
-      console.log(err)
-    }
-  }
-
-  async function handleSubmit(){
-
-    if(form.senha !== form.confirmarSenha){
+    if (form.senha !== form.confirmarSenha) {
       alert("As senhas não coincidem")
       return
     }
 
-    try{
+    try {
 
-      setLoading(true)
+      await alunoSchema.validate(form, { abortEarly: false })
 
-      const response = await axios.post(
-        "http://localhost:8080/alunos",
-        form
-      )
+      setErros({})
 
-      console.log(response.data)
+    } catch (error: any) {
 
-      alert("Aluno cadastrado com sucesso!")
+      const errosFormatados: Record<string, string> = {}
+
+      error.inner.forEach((err: any) => {
+        errosFormatados[err.path] = err.message
+      })
+
+      setErros(errosFormatados)
+
+      return
+    }
+
+    try {
+      const alunoData = {
+        matricula: Date.now().toString(),
+        periodo: Number(form.periodo),
+        pessoa: {
+          cpf: form.cpf,
+          nome: form.nome,
+          dataNascimento: form.dataNascimento,
+          logradouro: form.logradouro,
+          numero: Number(form.numero),
+          bairro: form.bairro,
+          cidade: form.cidade,
+          estado: form.estado,
+          cep: form.cep
+        },
+        usuario: {
+          email: form.email,
+          tipoUsuario: "Aluno",
+          password: form.senha
+        }
+      }
+
+      await criarAluno(alunoData)
+      setAlerta({
+        tipo: "success",
+        mensagem: "Que beleza, seu aluno foi cadastrado com sucesso!"
+      })
 
       setForm(initialForm)
 
-    }catch(err){
+    } catch (err) {
       console.error(err)
-      alert("Erro ao cadastrar aluno")
-    }finally{
-      setLoading(false)
+      setAlerta({
+        tipo: "error",
+        mensagem: "Opss, parece que algo deu errado durante o cadastro do aluno." // precisamos mapear esse erro depois no back pra vir do response by João Pedro Vidal
+      })
     }
-
   }
 
-  return(
+  return (
     <Container>
+      <Stack mt={2} gap={2} component='div'>
+        <Stack>
+          <Card.Root>
+            <Card.Header>
+              <Card.Title>Dados Pessoais</Card.Title>
+            </Card.Header>
+            <Card.Content>
+              <Grid container spacing={1}>
 
-      <Card.Root>
+                <Grid size={6}>
+                  <TextField
+                    required
+                    label="Nome"
+                    name="nome"
+                    value={form.nome}
+                    error={!!erros.nome}
+                    helperText={erros.nome}
+                    onChange={handleChange}
+                  />
+                </Grid>
 
-        <Card.Header>
-          <Card.Title>Dados pessoais</Card.Title>
-        </Card.Header>
+                <Grid size={3}>
+                  <TextField
+                    required
+                    label="CPF"
+                    name="cpf"
+                    value={form.cpf}
+                    error={!!erros.cpf}
+                    helperText={erros.cpf}
+                    onChange={handleChange}
+                  />
+                </Grid>
 
-        <Card.Content>
-          <Grid container spacing={4}>
+                <Grid size={3}>
+                  <TextField
+                    required
+                    label="Data Nascimento"
+                    name="dataNascimento"
+                    type="date"
+                    value={form.dataNascimento}
+                    error={!!erros.dataNascimento}
+                    helperText={erros.dataNascimento}
+                    onChange={handleChange}
+                    InputLabelProps={{ shrink: true }}
+                  />
+                </Grid>
 
-            <Grid size={6}>
-              <TextField label="Nome" name="nome" value={form.nome} onChange={handleChange}/>
-            </Grid>
+                <Grid size={6}>
+                  <TextField
+                    required
+                    label="Logradouro"
+                    name="logradouro"
+                    value={form.logradouro}
+                    error={!!erros.logradouro}
+                    helperText={erros.logradouro}
+                    onChange={handleChange}
+                  />
+                </Grid>
 
-            <Grid size={3}>
-              <TextField label="CPF" name="cpf" value={form.cpf} onChange={handleChange}/>
-            </Grid>
+                <Grid size={2}>
+                  <TextField
+                    required
+                    label="Número"
+                    name="numero"
+                    value={form.numero}
+                    error={!!erros.numero}
+                    helperText={erros.numero}                    
+                    onChange={handleChange}
+                  />
+                </Grid>
 
-            <Grid size={3}>
-              <TextField
-                label="Data Nascimento"
-                name="dataNascimento"
-                type="date"
-                value={form.dataNascimento}
-                onChange={handleChange}
-                InputLabelProps={{ shrink: true }}
-              />
-            </Grid>
+                <Grid size={4}>
+                  <TextField
+                    required
+                    label="Bairro"
+                    name="bairro"
+                    value={form.bairro}
+                    error={!!erros.bairro}
+                    helperText={erros.bairro}                    
+                    onChange={handleChange}
+                  />
+                </Grid>
 
-            <Grid size={6}>
-              <TextField label="Logradouro" name="logradouro" value={form.logradouro} onChange={handleChange}/>
-            </Grid>
+                <Grid size={4}>
+                  <TextField
+                    required
+                    label="Cidade"
+                    name="cidade"
+                    value={form.cidade}
+                    error={!!erros.cidade}
+                    helperText={erros.cidade}                    
+                    onChange={handleChange}
+                  />
+                </Grid>
 
-            <Grid size={2}>
-              <TextField label="Número" name="numero" value={form.numero} onChange={handleChange}/>
-            </Grid>
+                <Grid size={2}>
+                  <TextField
+                    required
+                    label="Estado"
+                    name="estado"
+                    value={form.estado}
+                    error={!!erros.estado}
+                    helperText={erros.estado}                    
+                    onChange={handleChange}
+                  />
+                </Grid>
 
-            <Grid size={4}>
-              <TextField label="Bairro" name="bairro" value={form.bairro} onChange={handleChange}/>
-            </Grid>
+                <Grid size={2}>
+                  <TextField
+                    required
+                    label="CEP"
+                    name="cep"
+                    value={form.cep}
+                    error={!!erros.cep}
+                    helperText={erros.cep}                    
+                    onChange={(e) => {
+                      handleChange(e)
+                    }}
+                  />
+                </Grid>
 
-            <Grid size={4}>
-              <TextField label="Cidade" name="cidade" value={form.cidade} onChange={handleChange}/>
-            </Grid>
+                <Grid size={2}>
+                  <IconButton
+                    color="primary"
+                    onClick={buscarEnderecoPeloCep}
+                  >
+                    <MapPin size={22} />
+                  </IconButton>
+                </Grid>
 
-            <Grid size={2}>
-              <TextField label="Estado" name="estado" value={form.estado} onChange={handleChange}/>
-            </Grid>
+              </Grid>
+            </Card.Content>
+          </Card.Root>
+        </Stack>
 
-            <Grid size={2}>
-              <TextField
-                label="CEP"
-                name="cep"
-                value={form.cep}
-                onChange={(e)=>{
-                  handleChange(e)
-                  buscarCep(e.target.value)
-                }}
-              />
-            </Grid>
+        <Stack component='div'>
+          <Card.Root>
+            <Card.Header>
+              <Card.Title>Dados de Usuário</Card.Title>
+            </Card.Header>
 
-          </Grid>
-        </Card.Content>
+            <Card.Content>
+              <Grid container spacing={1}>
+
+                <Grid size={6}>
+                  <TextField
+                    required
+                    label="Email"
+                    name="email"
+                    value={form.email}
+                    error={!!erros.email}
+                    helperText={erros.email}                    
+                    onChange={handleChange}
+                  />
+                </Grid>
+
+                <Grid size={3}>
+                  <TextField
+                    required
+                    label="Senha"
+                    type="password"
+                    name="senha"
+                    value={form.senha}
+                    error={!!erros.senha}
+                    helperText={erros.senha}
+                    onChange={handleChange}
+                  />
+                </Grid>
+
+                <Grid size={3}>
+                  <TextField
+                    required
+                    label="Confirme a senha"
+                    type="password"
+                    name="confirmarSenha"
+                    value={form.confirmarSenha}
+                    error={!!erros.confirmarSenha}
+                    helperText={erros.confirmarSenha}                    
+                    onChange={handleChange}
+                  />
+                </Grid>
+
+              </Grid>
+            </Card.Content>
+          </Card.Root>
+        </Stack>
+
+        <Stack>
+          <Card.Root>
+
+            <Card.Header>
+              <Card.Title>Dados do Curso</Card.Title>
+            </Card.Header>
+
+            <Card.Content>
+              <Grid container spacing={1}>
+
+                <Grid size={4}>
+                  <TextField
+                    required
+                    label="Curso"
+                    name="curso"
+                    value={form.curso}
+                    error={!!erros.curso}
+                    helperText={erros.curso}                    
+                    onChange={handleChange}
+                  />
+                </Grid>
+
+                <Grid size={2}>
+                  <TextField
+                    required
+                    label="Periodo"
+                    name="periodo"
+                    value={form.periodo}
+                    error={!!erros.periodo}
+                    helperText={erros.periodo}                    
+                    onChange={handleChange}
+                  />
+                </Grid>
+
+              </Grid>
+            </Card.Content>
+          </Card.Root>
+        </Stack>
 
 
-        <Card.Header>
-          <Card.Title>Dados de Usuário</Card.Title>
-        </Card.Header>
 
-        <Card.Content>
-          <Grid container spacing={4}>
-
-            <Grid size={6}>
-              <TextField label="Email" name="email" value={form.email} onChange={handleChange}/>
-            </Grid>
-
-            <Grid size={3}>
-              <TextField
-                label="Senha"
-                type="password"
-                name="senha"
-                value={form.senha}
-                onChange={handleChange}
-              />
-            </Grid>
-
-            <Grid size={3}>
-              <TextField
-                label="Confirme a senha"
-                type="password"
-                name="confirmarSenha"
-                value={form.confirmarSenha}
-                onChange={handleChange}
-              />
-            </Grid>
-
-          </Grid>
-        </Card.Content>
-
-
-        <Card.Header>
-          <Card.Title>Dados do curso</Card.Title>
-        </Card.Header>
-
-        <Card.Content>
-          <Grid container spacing={4}>
-
-            <Grid size={4}>
-              <TextField label="Curso" name="curso" value={form.curso} onChange={handleChange}/>
-            </Grid>
-
-            <Grid size={2}>
-              <TextField label="Periodo" name="periodo" value={form.periodo} onChange={handleChange}/>
-            </Grid>
-
-          </Grid>
-        </Card.Content>
-
-      </Card.Root>
-
-
-      <Stack
-        display="flex"
-        flexDirection="row"
-        justifyContent="flex-end"
-        mt="4px"
-      >
-
-        <Button
-          variant="contained"
-          sx={{width:"120px", height:"35px"}}
-          onClick={handleSubmit}
-          disabled={loading}
+        <Stack
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-between"
+          gap={2}
         >
-          {loading ? "Salvando..." : "Cadastrar"}
-        </Button>
-
+          {alerta && (
+            <Alert
+              severity={alerta.tipo}
+              sx={{
+                width: '100%',
+                height: '35px',
+                display: 'flex',
+                alignItems: 'center'
+              }}
+            >
+              {alerta.mensagem}
+            </Alert>
+          )}
+          <Button
+            variant="contained"
+            sx={{ width: "90px", height: "35px" }}
+            onClick={handleSubmit}
+            isLoading={carregando}
+          >
+            Cadastrar
+          </Button>
+        </Stack>
       </Stack>
-
     </Container>
   )
 }
---
+++ frontend/src/models/usuario-model.ts
@@ -0,0 +1,5 @@
+export interface Usuario {
+  email: string
+  tipoUsuario: string
+  password: string
+}
