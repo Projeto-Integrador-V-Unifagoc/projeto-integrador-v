3a03244de3bb1a56b34d190022147cb1899b8e66
Jhonathan Freitas <freitasjhonathan00@gmail.com>
2026-04-06 12:55:17 -0300

    refactor: ajuste base_url no frontend

--- frontend/src/services/auth-services.ts
+++ frontend/src/services/auth-services.ts
@@ -2,14 +2,22 @@ import api from './conexao-api';
 import type { Usuario } from '../models/usuario';
 
 export const authService = {
-  async cadastrar(dados: any) {
-    const response = await api.post('/usuarios', dados); // Ou '/cadastro'
+  async cadastrar(dados: Usuario) {
+    const response = await api.post('/cadastro', dados); 
     return response.data;
   },
 
-  async login(dados: any) {
-    const response = await api.post('http://localhost:3000/login', dados);
+  async login(dados: Pick<Usuario, 'email' | 'senha'>) {
+    const response = await api.post('/login', dados);
+    return response.data;
+  },
+
+  async getMe(token: string) {
+    const response = await api.get('/me', {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
     return response.data;
   }
-
 };
