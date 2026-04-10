cdca8eac8312d24d41aeb749de0dfab57454dfeb
Joao Pedro <ppvidalsantos@gmail.com>
2026-04-10 20:43:14 -0300

    feat: adiciona responsividade ao componente de SearchTextField

--- frontend/src/components/SearchTextField/SearchTextField.tsx
+++ frontend/src/components/SearchTextField/SearchTextField.tsx
@@ -1,22 +1,30 @@
-import { useState, type ReactNode } from "react";
+import { 
+    useState, 
+    type ReactNode 
+} from "react";
+
+import { useNavigate } from "react-router-dom";
 
 import {
     IconButton,
     InputAdornment,
     Stack,
     Typography,
+    useMediaQuery,
+    useTheme,
 } from "@mui/material";
 
+import type { Periodos } from "../../enums/periodos";
+import type { Cursos } from "../../enums/cursos";
+
+import DropDownPeriodos from "../DropDownPeriodos/DropDownsPeriodos";
 import DropDownCursos from "../DropDownCursos/DropDownCursos";
 import { FilterMenu } from "../FilterMenu/FilterMenu";
-import type { Cursos } from "../../enums/cursos";
 import TextField from "../TextField";
 import Button from "../Button";
 
 import { ListFilter, Search } from "lucide-react";
-import { useNavigate } from "react-router-dom";
-import DropDownPeriodos from "../DropDownPeriodos/DropDownsPeriodos";
-import type { Periodos } from "../../enums/periodos";
+
 
 interface SearchTextFieldProps {
     children: ReactNode
@@ -27,7 +35,9 @@ export default function SearchTextField(props: SearchTextFieldProps) {
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
     const [curso, setCurso] = useState<Cursos | ''>('')
     const [periodo, setPeiodo] = useState<Periodos | ''>('')
-    
+    const theme = useTheme()
+    const isMobile = useMediaQuery(theme.breakpoints.down("sm"))
+
     const navigate = useNavigate()
 
     function navegarPaginaCadastro(){
@@ -48,15 +58,20 @@ export default function SearchTextField(props: SearchTextFieldProps) {
         <>
             <Stack
                 display='flex'
-                alignItems='center'
-                flexDirection='row'
+                alignItems={isMobile ? "flex-start" : "center"}
+                flexDirection={isMobile ? "column" : "row"}
                 gap={1}
                 pt={1}
             >
-                <Typography fontWeight='bold' variant="subtitle2">{children}</Typography>
+                <Typography 
+                    fontWeight='bold' 
+                    variant="subtitle2"
+                >
+                    {children}
+                </Typography>
                 <TextField
                     variant="outlined"
-                    placeholder="Pesquisar alunos"
+                    placeholder="Pesquisar Alunos"
                     fullWidth
                     InputProps={{
                         endAdornment: (
@@ -78,9 +93,18 @@ export default function SearchTextField(props: SearchTextFieldProps) {
                         "& .MuiOutlinedInput-root": {
                             borderRadius: '17px',
                         },
+                        width: isMobile ? "100%" : "auto",
                     }}
                 />
-                <Button variant="contained" sx={{ width: '80px' }} onClick={navegarPaginaCadastro}>Adicionar</Button>
+                <Button 
+                    variant="contained" 
+                    onClick={navegarPaginaCadastro}
+                    sx={{ 
+                        width: isMobile ? "100%" : "80px"  
+                    }} 
+                >
+                    Adicionar
+                </Button>
             </Stack>
             <FilterMenu.Root
                 open={open}
