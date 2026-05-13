ef379bb0dbea6b20c63e2c9307eece5b68788d78
Nathalia <nathalialourencobeniaparecida@gmail.com>
2026-05-12 21:10:09 -0300

    feat: implementação get

---
+++ backend/src/Modules/frequencia/controller/FrequenciaController.ts
@@ -0,0 +1,91 @@
+import { FrequenciaService } from "../service/FrequenciaService";
+
+export class FrequenciaController {
+  frequenciaService = new FrequenciaService();
+
+  async listarOpcoes(req: any, res: any) {
+    try {
+      res.status(200).json(await this.frequenciaService.listarOpcoes(req));
+    } catch (error) {
+      res.status(400).json({ error: (error as Error).message });
+    }
+  }
+
+  async obterChamada(req: any, res: any) {
+    try {
+      res
+        .status(200)
+        .json(await this.frequenciaService.obterChamada(req.query.turmaId, req.query.data, req));
+    } catch (error) {
+      res.status(400).json({ error: (error as Error).message });
+    }
+  }
+
+  async registrarFrequencia(req: any, res: any) {
+    try {
+      res.status(201).json(await this.frequenciaService.registrarFrequencia(req.body, req));
+    } catch (error) {
+      res.status(400).json({ error: (error as Error).message });
+    }
+  }
+
+  async editarFrequencia(req: any, res: any) {
+    try {
+      res
+        .status(200)
+        .json(await this.frequenciaService.editarFrequencia(req.params.id, req.body, req));
+    } catch (error) {
+      res.status(400).json({ error: (error as Error).message });
+    }
+  }
+
+  async removerFrequencia(req: any, res: any) {
+    try {
+      res.status(200).json(await this.frequenciaService.removerFrequencia(req.params.id, req));
+    } catch (error) {
+      res.status(400).json({ error: (error as Error).message });
+    }
+  }
+
+  async consultarAluno(req: any, res: any) {
+    try {
+      res.status(200).json(await this.frequenciaService.consultarAluno(req.params.alunoId, req));
+    } catch (error) {
+      res.status(400).json({ error: (error as Error).message });
+    }
+  }
+
+  async consultarTurma(req: any, res: any) {
+    try {
+      res
+        .status(200)
+        .json(await this.frequenciaService.consultarTurma(req.params.turmaId, req, req.query));
+    } catch (error) {
+      res.status(400).json({ error: (error as Error).message });
+    }
+  }
+
+  async registrarJustificativa(req: any, res: any) {
+    try {
+      res
+        .status(200)
+        .json(
+          await this.frequenciaService.registrarJustificativa(
+            req.params.id,
+            req.body.justificativa,
+            req
+          )
+        );
+    } catch (error) {
+      res.status(400).json({ error: (error as Error).message });
+    }
+  }
+
+  async gerarRelatorio(req: any, res: any) {
+    try {
+      res.status(200).json(await this.frequenciaService.gerarRelatorio(req.query, req));
+    } catch (error) {
+      res.status(400).json({ error: (error as Error).message });
+    }
+  }
+}
