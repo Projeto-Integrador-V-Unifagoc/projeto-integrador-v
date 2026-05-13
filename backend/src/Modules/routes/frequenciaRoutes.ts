7880c29a3a952ebde593a79839bdf602d9cc13ec
Theilor Martins <martins.theilor8@gmail.com>
2026-05-13 08:45:17 -0300

    feat: adiciona consultas get de frequencia

--- backend/src/Modules/routes/frequenciaRoutes.ts
+++ backend/src/Modules/routes/frequenciaRoutes.ts
@@ -4,4 +4,9 @@ import { FrequenciaController } from "../frequencia/controller/FrequenciaControl
 const frequenciaController = new FrequenciaController();
 export const frequenciaRouter = Router();
 
+frequenciaRouter.get("/opcoes", (req, res) => frequenciaController.listarOpcoes(req, res));
+frequenciaRouter.get("/chamada", (req, res) => frequenciaController.obterChamada(req, res));
+frequenciaRouter.get("/relatorio", (req, res) => frequenciaController.gerarRelatorio(req, res));
 frequenciaRouter.post("/", (req, res) => frequenciaController.registrarFrequencia(req, res));
+frequenciaRouter.get("/aluno/:alunoId", (req, res) => frequenciaController.consultarAluno(req, res));
+frequenciaRouter.get("/turma/:turmaId", (req, res) => frequenciaController.consultarTurma(req, res));
